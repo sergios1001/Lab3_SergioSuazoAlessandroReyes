101dd474f15789913644734523b78bbd65d4a58c
agreyest <56709605+agreyest@users.noreply.github.com>
2020-02-07 14:35:20 -0600

    main
    avance en la creacion del main

--- Lab3_SergioAlessandro/src/lab3_sergioalessandro/Lab3_SergioAlessandro.java
+++ Lab3_SergioAlessandro/src/lab3_sergioalessandro/Lab3_SergioAlessandro.java
@@ -30,8 +30,14 @@ public class Lab3_SergioAlessandro {
     
     public static Scanner leer = new Scanner(System.in);
     public static Random random = new Random();
-    ArrayList<Persona> alibres = new ArrayList();
     
+    ArrayList<Jugador> jugadores = new ArrayList();
+    ArrayList<Jugador> libresj = new ArrayList();
+    ArrayList<Entrenador> entrenadores = new ArrayList();
+    ArrayList<Entrenador> librese = new ArrayList();
+    ArrayList<Medico> medicos = new ArrayList();
+    ArrayList<Equipo> equipos = new ArrayList();
+    ArrayList<Jugada> jugada = new ArrayList();
     
     
     
@@ -53,33 +59,63 @@ public class Lab3_SergioAlessandro {
         char resp = 's';
         while (resp == 's') {
             System.out.println("Que opcion quiere utilizar:"
-                    + "\n1) Aregar"
+                    + "\n1) Agregar"
                     + "\n2) Despedir"
                     + "\n3) Modificar"
                     + "\n4) Simular entreno"
                     + "\n5) Simular jugar"
                     + "\n6) Salir"
-                    + "\nSeleccione uno: ");
+                    + "\nSeleccione una opción(si pone una opcion no indicada, el programa termina): ");
             int op = leer.nextInt();
             switch (op) {
-                case 1:
+                case 1://Agrear
+                    System.out.println("Que desea crear: "
+                            + "\na) Jugador"
+                            + "\nb) Medico"
+                            + "\nc) Dueño"
+                            + "\nd) Entrenador"
+                            + "\ne) Equipo"
+                            + "\nf) jugada"
+                            + "\nSeleccione una opcion: ");
+                    char opm1 = leer.next().charAt(0);
+                    switch(opm1){
+                        case 'a':
+                            
+                            break;
+                        case 'b':
+                            
+                            break;
+                        case 'c':
+                            
+                            break;
+                        case 'd':
+                            
+                            break;
+                        case 'e':
+                            
+                            break;
+                        case 'f':
+                            
+                            break;
+                    }
+                    break;
+                case 2://despedir
 
                     break;
-                case 2:
+                case 3://modificar
 
                     break;
-                case 3:
+                case 4://simular entreno
 
                     break;
-                case 4:
+                case 5://simular jugar
 
                     break;
-                case 5:
-
-                    break;
-                case 6:
+                case 6://salir
                     resp = 'n';
                     break;
+                default:
+                    resp = 'n';
             }
         }
 
